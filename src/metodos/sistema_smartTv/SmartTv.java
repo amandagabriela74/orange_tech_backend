483b8b51128411d65e13461602e65183b6c86f67
amandagabriela74 <amandagabriela056@gmail.com>
2023-01-13 22:36:52 -0300

    praticando métodos, criação sistema smartTV

---
+++ src/metodos/sistema_smartTv/SmartTv.java
@@ -0,0 +1,31 @@
+package metodos.sistema_smartTv;
+
+public class SmartTv {
+	boolean ligada = false;
+	int canal = 1;
+	int volume = 15;
+	
+	public void ligar() {
+		ligada = true;
+	}
+	
+	public void desligar() {
+		ligada = false;
+	}
+	
+	public void passarCanal() {
+		canal++;
+		System.out.println("Você passou para o canal: " + canal);
+	}
+	
+	public void voltarCanal() {
+		canal--;
+		System.out.println("Você voltou para o canal: " + canal);
+	}
+	
+	//alterar canal definindo o número correspondente.
+	public void alterarCanal(int novoCanal) {
+		canal = novoCanal;
+		System.out.println("Você alterou o canal para: " + canal);
+	}
+}
